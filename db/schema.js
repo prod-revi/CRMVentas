8462264fbaab11663009dbddbf9f0da44ee0574b
Revi <oliiv@outlook.com>
2020-05-03 11:56:46 -0300

    Conectandose al servidor

--- db/schema.js
+++ db/schema.js
@@ -2,22 +2,9 @@ const { gql } = require('apollo-server');
 
 // Schema
 const typeDefs = gql`
-  type Curso {
-    titulo: String
-    tecnologia: String
-  }
-
-  type Tecnologia {
-    tecnologia: String
-  }
-
-  input CursoInput {
-    tecnologia: String
-  }
 
   type Query {
-    obtenerCursos(input: CursoInput!) : [Curso]
-    obtenerTecnologia: [Tecnologia]
+    obtenerCursos: String
   }
 `;
 
--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
 
+const conectarDB = require('./config/db');
+
+// Conectar a la base de datos
+conectarDB();
+
 // Server 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => {
-    const usuarioId = 20;
-    return {usuarioId};
-  }
+  context: () => {}
 });
 
 // Run Server
