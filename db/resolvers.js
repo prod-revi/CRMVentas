7582b3c207596181573a23583dea9007760d4b2b
Revi <oliiv@outlook.com>
2020-05-03 12:53:06 -0300

    Creando Usuarios

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,8 +1,35 @@
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+
 // Resolvers
 const resolvers = {
   Query: {
-    obtenerCursos: (_, {}, ctx, info) => {
-      return "algo";
+    obtenerCursos: () => 'Algo'
+  },
+  Mutation: {
+    nuevoUsuario: async(_, { input } ) => {
+      
+      const { email, password } = input;
+
+      // Revisar si el usuario ya esta registrado
+      const existeUsuario = await Usuario.findOne({email});
+      if (existeUsuario) {
+        throw new Error('El usario ya esta registrado');
+      }
+
+      // Hashear su password
+
+      const salt = await bcryptjs.genSalt(10);
+      input.password = await bcryptjs.hash(password, salt);
+      
+      try {
+        // Guardarlo en la base de datos
+        const usuario = new Usuario(input);
+        usuario.save();
+        return usuario;
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 }
